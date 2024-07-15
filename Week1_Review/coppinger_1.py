22543a61298088c6ad53e75a7934efa522fea5af
Coppinger <bjcoppinger@email.neit.edu>
2024-07-15 10:26:01 -0400

    W1 Review1

---
+++ Week1_Review/coppinger_1.py
@@ -0,0 +1,12 @@
+#Comment BROSKI, you member
+#---------------------------
+#Your name, lab number, date
+#Variable dictionary
+#-----------------------
+
+#---IMPORTS-------------------------------------
+
+#----Functions---------------------
+
+#-----Main Code-----------------------------
+print ("Hello World")
